2d151b11c6f744cba35dd5a43cda4a78a9927639
Danuta Brzezinska <300634753+danuta-b-data@users.noreply.github.com>
2026-07-12 23:28:10 +0200

    Fix funnel stage labels to user-friendly names in mart_funnel_overview

--- models/marts/mart_funnel_overview.sql
+++ models/marts/mart_funnel_overview.sql
@@ -4,7 +4,7 @@ with funnel as (
 
 aggregated as (
     select
-        '1_signed_up'           as funnel_stage,
+        'Signed Up'             as funnel_stage,
         count(*)                as user_count
     from funnel
     where did_signup = 1
@@ -12,7 +12,7 @@ aggregated as (
     union all
 
     select
-        '2_created_design'      as funnel_stage,
+        'Created Design'        as funnel_stage,
         count(*)                as user_count
     from funnel
     where did_create_design = 1
@@ -20,7 +20,7 @@ aggregated as (
     union all
 
     select
-        '3_exported_design'     as funnel_stage,
+        'Exported Design'       as funnel_stage,
         count(*)                as user_count
     from funnel
     where did_export_design = 1
@@ -28,7 +28,7 @@ aggregated as (
     union all
 
     select
-        '4_invited_team'        as funnel_stage,
+        'Invited Team Member'   as funnel_stage,
         count(*)                as user_count
     from funnel
     where did_invite_team = 1
@@ -36,7 +36,7 @@ aggregated as (
     union all
 
     select
-        '5_upgraded'            as funnel_stage,
+        'Upgraded Plan'         as funnel_stage,
         count(*)                as user_count
     from funnel
     where did_upgrade = 1
@@ -47,15 +47,15 @@ with_conversion as (
         funnel_stage,
         user_count,
         first_value(user_count) over (
-            order by funnel_stage
+            order by user_count desc
         )                       as top_of_funnel,
         round(
             user_count * 100.0 / first_value(user_count) over (
-                order by funnel_stage
+                order by user_count desc
             ), 1
         )                       as conversion_rate_pct
     from aggregated
 )
 
 select * from with_conversion
-order by funnel_stage
+order by user_count desc
